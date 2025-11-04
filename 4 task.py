a43cf60f9f19423c8485a3a2c5aae803dedce687
Brayan Borba <bborba002@gmail.com>
2025-11-05 03:22:01 +1300

    Update 4 task.py

--- 4 task.py
+++ 4 task.py
@@ -1,2 +1,9 @@
-msg = input('Write your name: ')
-print('Nice to meet you,',msg)
+start = int(input("Enter the smaller number: "))
+end = int(input("Enter the larger number: "))
+
+total = 0
+
+for i in range(start, end + 1):
+    total += i ** 2
+
+print(total)
