bceeed817d40e05a174933f110f216c195a600cd
Brayan Borba <bborba002@gmail.com>
2025-11-05 03:16:57 +1300

    Update 10 task.py

---
+++ 10 task.py
@@ -0,0 +1,7 @@
+guees = ("What is my favourite food?")
+print(guees)
+pao = input('Guess? ')
+while pao != "electricity":
+  print('Not even close.')
+  pao = input("Guess? ")
+print("You guessed it! Buzzzz")
